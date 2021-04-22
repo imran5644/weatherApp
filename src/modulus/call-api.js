bb451a6ada1877874e50a7b4e577293400dd523b
imran5644 <imi56444@gmail.com>
2021-04-23 00:37:58 +0800

    Add createCard and removeCard function

--- src/modulus/call-api.js
+++ src/modulus/call-api.js
@@ -1,24 +1,23 @@
-async function fetchWeather(){
-    const city = document.getElementById('city');
-    const temp = document.getElementById('temp');
-    const wind = document.getElementById('wind');
-    const humid = document.getElementById('humid');
+import {createCard, removeCard} from './dom-manipulation'
+
+async function fetchWeather(search){
+    
 
     const response = await fetch(`http://api.weatherbit.io/v2.0/current?key=9d79f0dae0494f8f89edd9f03706c15c&city=${search}`,
      { mode: "cors" });
      
      const weather = await response.json();
-     console.log('weather.data[0]');
-     city.textContent = weather.data[0].city_name;
-     temp.textContent = `temperature: ${weather.data[0].temp};`
-     wind.textContent = `wind speed: ${weather.data[0].wind_spd};`
-     humid.textContent = `humidity: ${weather.data[0].rh};`
+     console.log(weather.data[0]);
+     removeCard();
+
+     createCard(weather.data[0].city_name, weather.data[0].temp, weather.data[0].rh, weather.data[0].wind_spd);
 }
 
 function searchCity() {
     const input = document.querySelector('input');
     const button = document.querySelector('button');
 
+    
     button.addEventListener('click', () => {
         fetchWeather(input.value);
     });
---
+++ src/modulus/dom-manipulation.js
@@ -0,0 +1,66 @@
+const background = document.querySelector('#background');
+
+function removeCard(){
+    while(background.firstChild){
+        background.firstChild.remove();
+    }
+}
+
+function createCard(city, temperature, humidity, wind) {
+    const section = document.createElement("div")
+    section.classList.add("uk-section")
+  
+    const div = document.createElement("div")
+  
+    const cardParent = document.createElement("div")
+    cardParent.classList.add("uk-card")
+    cardParent.classList.add("uk-card-default")
+  
+    const cardHeader = document.createElement("div")
+    cardHeader.classList.add("uk-card-header")
+  
+    const cityHeading = document.createElement("div")
+    cityHeading.classList.add("uk-card-title")
+    cityHeading.setAttribute("id", "city")
+    cityHeading.innerHTML = city
+  
+    const cardBody = document.createElement("div")
+    cardBody.classList.add("uk-card-body")
+  
+    const tempParagraph = document.createElement("p")
+    tempParagraph.setAttribute("id", "temp")
+    tempParagraph.innerHTML = "Temperature:" + temperature
+  
+    const humidityParagraph = document.createElement("p")
+    humidityParagraph.setAttribute("id", "humid")
+    humidityParagraph.innerHTML = "Humidity:" + humidity
+  
+    const windParagraph = document.createElement("p")
+    windParagraph.setAttribute("id", "wind")
+    windParagraph.innerHTML = "Wind speed:" + wind
+  
+    const cardFooter = document.createElement("div")
+    cardFooter.classList.add("uk-card-footer")
+  
+    const unitSwitch = document.createElement("p")
+    unitSwitch.innerHTML = "Change F to C"
+  
+
+    background.appendChild(section)
+    section.appendChild(div)
+    div.appendChild(cardParent)
+  
+    cardParent.appendChild(cardHeader)
+    cardHeader.appendChild(cityHeading)
+  
+    cardParent.appendChild(cardBody)
+    cardBody.appendChild(tempParagraph)
+    cardBody.appendChild(humidityParagraph)
+    cardBody.appendChild(windParagraph)
+  
+    cardParent.appendChild(cardFooter)
+    cardFooter.appendChild(unitSwitch)
+  }
+  
+
+  export { createCard, removeCard };
